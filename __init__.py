25e9dc53f3029bd677851bbe048acda12bb2ace8
luiztropical <luiztropical@hotmail.com>
2026-06-08 09:10:35 -0300

    http

--- __init__.py
+++ __init__.py
@@ -1,12 +1,1 @@
-import logging
-
-def _prepare_logging():
-
-    logger = logging.getLogger(__name__)
-    logger.addHandler(logging.NullHandler())
-
-    websocket_logger = logging.getLogger("websocket")
-    websocket_logger.setLevel(logging.DEBUG)
-    websocket_logger.addHandler(logging.NullHandler())
-
-_prepare_logging()
+"""Module for IQ Option API http resources."""
